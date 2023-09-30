0c5f333655163568562c84a60d70dd868ff55e7e
Amir Livne Bar-on <amir.livne.baron@gmail.com>
2023-09-30 22:57:15 +0300

    metal

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,25 @@ fn sky_color(direction: geometry::Vec3) -> Color {
 fn main() -> io::Result<()> {
     let mut scene = scene::Scene::new();
     scene.sky = Box::new(&sky_color);
-    scene.add_object(Sphere{ center: geometry::Vec3(0.0, 0.0, 1.0), radius: 0.5 }, material::Material{});
-    scene.add_object(Sphere{ center: geometry::Vec3(0.0, -101.0, 1.0), radius: 100.5 }, material::Material{});
+    scene.camera = camera::Camera {
+        position: geometry::Vec3(0.0, 0.0, -3.0),
+        focal_length: 4.0,
+    };
+
+    let material_ground = material::Material{albedo: Color{red: 0.8, green: 0.8, blue: 0.0}, reflective: false};
+    let material_center = material::Material{albedo: Color{red: 0.7, green: 0.3, blue: 0.3}, reflective: false};
+    let material_left   = material::Material{albedo: Color{red: 0.8, green: 0.8, blue: 0.8}, reflective: true};
+    let material_right  = material::Material{albedo: Color{red: 0.8, green: 0.6, blue: 0.2}, reflective: true};
+
+    scene.add_object(Sphere{ center: geometry::Vec3(0.0, -100.5, 1.0), radius: 100.0 }, material_ground);
+    scene.add_object(Sphere{ center: geometry::Vec3(0.0, 0.0, 1.0), radius: 0.5 }, material_center);
+    scene.add_object(Sphere{ center: geometry::Vec3(-1.0, 0.0, 1.0), radius: 0.5 }, material_left);
+    scene.add_object(Sphere{ center: geometry::Vec3(1.0, 0.0, 1.0), radius: 0.5 }, material_right);
 
     let tracer = tracing::Tracer {
         image_width: 600,
         aspect_ratio: 16.0 / 9.0,
-        samples_per_pixel: 100,
+        samples_per_pixel: 50,
     };
     let image = tracer.render(Arc::new(scene));
 
--- src/material.rs
+++ src/material.rs
@@ -1,10 +1,14 @@
 use crate::geometry::Ray;
+use crate::geometry::Vec3;
 use crate::geometry::dot;
 use crate::geometry::random_unit_vector;
 use crate::graphics::Color;
 use crate::scene::HitRecord;
 
-pub struct Material {}
+pub struct Material {
+    pub albedo: Color,
+    pub reflective: bool,
+}
 
 impl Material {
     pub fn scatter(&self, ray: &Ray, hit_record: &HitRecord) -> Option<(Color, Ray)> {
@@ -13,13 +17,24 @@ impl Material {
             return None
         }
 
-        let diffused_ray = Ray {
-            origin: hit_record.hit_point,
-            direction: hit_record.normal + random_unit_vector(),
-        };
+        let direction = self.scatter_direction(ray, hit_record);
         return Some((
-            Color{ red: 0.5, green: 0.5, blue: 0.5 },
-            diffused_ray,
+            self.albedo,
+            Ray { origin: hit_record.hit_point, direction },
         ))
+    }
+
+    fn scatter_direction(&self, incoming_ray: &Ray, hit_record: &HitRecord) -> Vec3 {
+        if self.reflective {
+            incoming_ray.direction - 2.0 * dot(incoming_ray.direction, hit_record.normal) * hit_record.normal
+        } else {
+            let lambertian = hit_record.normal + random_unit_vector();
+            if lambertian.norm2() < 1e-8 {
+                // Edge case, avoid scattering in near-zero direction
+                hit_record.normal
+            } else {
+                lambertian
+            }
+        }
     }
 }
